8982945ab26d42bc2a156759b6d720ef1c170085
ZahraKell <fatima_zohra.kellou@univ-constantine2.dz>
2026-05-17 22:34:50 +0100

    Fix CORS

--- backend1/config/settings.py
+++ backend1/config/settings.py
@@ -156,6 +156,21 @@ else:
     ).split(',')
     CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_METHODS = [
+    'DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 # ── Email ──────────────────────────────────────────────────
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = config('EMAIL_HOST',     default='smtp.gmail.com')
