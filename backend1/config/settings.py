0a2703b6250c80e873f17b54bad2be44ded016f0
ZahraKell <fatima_zohra.kellou@univ-constantine2.dz>
2026-05-18 00:10:00 +0100

    Fix

--- backend1/config/settings.py
+++ backend1/config/settings.py
@@ -176,7 +176,7 @@ EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = config('EMAIL_HOST',     default='smtp.gmail.com')
 EMAIL_PORT          = config('EMAIL_PORT',     default=587, cast=int)
 EMAIL_USE_TLS       = True
-EMAIL_TIMEOUT       = 30
+EMAIL_TIMEOUT = config('EMAIL_TIMEOUT', default=10, cast=int)
 EMAIL_HOST_USER     = config('EMAIL_HOST_USER',     default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL  = config('DEFAULT_FROM_EMAIL',  default=EMAIL_HOST_USER)
